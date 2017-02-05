483c2631ab75268ab490d508990b33d3c9e248f3
ano <250081373@qq.com>
2017-02-05 10:08:35 +0800

    add dev cpp project

---
+++ devc/x86/word2vec/word2vec_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef WORD2VEC_PRIVATE_H
+#define WORD2VEC_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.2.0.0"
+#define VER_MAJOR	0
+#define VER_MINOR	2
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.2.0.0"
+#define FILE_DESCRIPTION	"Google word2vec for Windows"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	"0.2.0.0"
+
+#endif /*WORD2VEC_PRIVATE_H*/
